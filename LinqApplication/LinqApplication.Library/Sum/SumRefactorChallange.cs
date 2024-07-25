db53688ba7578bdbb9ea0f8a63d9c2697fc4b6f6
Stefan Trenh <stefantrenh@hotmail.com>
2024-07-25 08:53:05 +0200

    added refactory challange for sum

--- LinqApplication/LinqApplication.Library/Sum/SumRefactorChallange.cs
+++ LinqApplication/LinqApplication.Library/Sum/SumRefactorChallange.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace LinqApplication.LinqApplication.Library.Sum
 {
-    internal class SumRefactorChallange
+    public class SumRefactorChallange
     {
+        //TODO implement this method
+        public static bool HasAnyStudentSumOfMarksLargerThan100_Refactored(IEnumerable<Student> students)
+        {
+            return students.Any(x => x.Marks.Sum() > 100); 
+        }
+
+        //do not modify this method
+        public static bool HasAnyStudentSumOfMarksLargerThan100(IEnumerable<Student> students)
+        {
+            foreach (var student in students)
+            {
+                var sumOfMarks = 0;
+                foreach (var mark in student.Marks)
+                {
+                    sumOfMarks += mark;
+                }
+                if (sumOfMarks > 100)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
+
+    public class Student
+    {
+        public IEnumerable<int> Marks { get; set; }
+    }
+
 }
