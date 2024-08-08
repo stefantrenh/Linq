22cedae17634a94e99563576fbdb1a23f21e45f5
Stefan Trenh <stefantrenh@hotmail.com>
2024-08-08 10:19:32 +0200

    Added Where + tests

---
+++ LinqApplication/LinqApplication.Library/TakeExercise1/TakeExcercise2.cs
@@ -0,0 +1,18 @@
+﻿using LinqApplication.LinqApplication.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApplication.LinqApplication.Library.TakeExercise1
+{
+    public class TakeExcercise2
+    {
+        public IEnumerable<Pet> GetGivenPercentOfHeaviestPets(IEnumerable<Pet> pets, int percent)
+        {
+            return pets.OrderByDescending(pet => pet.Weight)
+                       .Take(pets.Count() * percent);
+        }
+    }
+}
---
+++ LinqApplication/LinqApplication.Library/TakeExercise1/TakeExercise1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApplication.LinqApplication.Library.TakeExercise1
+{
+    /*
+     Implement the TakeSome method, which returns a certain part of the collection, according to this algorithm:
+
+        if there are less than 10 elements in the collection, 
+            it returns 3 elements (or all of them, if there are less than 3 elements in the collection)
+
+        if there are 10 or more, but less than 100 elements in the collection,
+            it returns 30 elements (or all of them, if there are less than 30 elements in the collection)
+
+        if there are 100 or more elements, 
+            it returns all elements
+     */
+    public class TakeExercise1
+    {
+        public static IEnumerable<int> TakeSome(IEnumerable<int> numbers)
+        {
+            return numbers.Count() < 10 ?
+                numbers.Take(3) :
+                numbers.Count() < 100 ? numbers.Take(30) : numbers;
+        }
+    }
+}
